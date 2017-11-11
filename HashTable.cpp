5cc26edb9fd94f04a37a118d0b8621721528f8e7
Tri Doan <tridoan1998@gmail.com>
2017-11-10 19:35:58 -0800

    Add files via upload

---
+++ HashTable.cpp
@@ -0,0 +1,23 @@
+/*
+ * HashTable.cpp
+ *
+ *  Created on: Nov 7, 2017
+ *      Author: $$$$$$$$$$$$$$$$$$$$
+ */
+
+
+
+#include "Book.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "HashTable.h"
+using namespace std;
+
+HashTable::HashTable()
+{
+
+}
+
+
+Book::Book():title(""), author(""), price(0.0), isbn(0){};
---
+++ HashTest.cpp
@@ -0,0 +1,14 @@
+/*
+ * HashTest.cpp
+ *
+ *  Created on: Nov 7, 2017
+ *      Author: $$$$$$$$$$$$$$$$$$$$
+ */
+
+#include "Book.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "HashTable.h"
+
+
